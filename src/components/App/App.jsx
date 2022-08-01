65f25aa6722f21c8092c021e0c5f87f03c8362a6
Abhijit Mali <maliabhijit183@gmail.com>
2022-08-01 14:21:26 +0530

    update 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Router, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './../Home/Home'
 import Contact from './../Contact/Contact'
